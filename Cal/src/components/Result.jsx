0e747993e53dd3e70be327b07ecf38da7f9569cf
Emerson Naranjo <c_enaranjo@medicalcardsystem.com>
2022-08-19 08:57:08 -0500

    Init Commit

---
+++ Cal/src/components/Result.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Result = (ourProperties) => {
+    debugger;
+    console.log(ourProperties);
+    return (
+            <div className="result">
+                <span>0</span>
+            </div>)
+};
+
+export default Result;
